30bb09ee9ec94a6b02fd1e5a7fe3b84d8fecda30
marc-henrard <marc@opengamma.com>
2016-11-08 17:53:20 +0100

    Bachelier implied volatility: implemented efficient approximation from the paper "Fast and Accurate Analytic Basis Point Volatility" by Fabien Le Floc’h.

---
+++ src/main/java/marc/henrard/risq/model/bachelier/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2016 - Marc Henrard.
+ */
+
+/**
+ * Package with classes related to the Bachelier or normal model for options.
+ */
+package marc.henrard.risq.model.bachelier;
+
