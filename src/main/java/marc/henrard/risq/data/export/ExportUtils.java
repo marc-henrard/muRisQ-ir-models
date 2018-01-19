d659bda880483535b80fe030d9034bd679789e67
Marc Henrard <marc@opengamma.com>
2018-01-19 12:31:46 +0100

    Enhancements to the ExportUtils. Adding CurrencyParameterSensitivities.

--- src/main/java/marc/henrard/risq/data/export/ExportUtils.java
+++ src/main/java/marc/henrard/risq/data/export/ExportUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016 - Marc Henrard.
+ * Copyright (C) 2016 - present by Marc Henrard.
  */
 package marc.henrard.risq.data.export;
 
@@ -7,11 +7,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.common.collect.ImmutableList;
+import com.opengamma.strata.collect.ArgChecker;
+import com.opengamma.strata.collect.array.DoubleArray;
+import com.opengamma.strata.collect.io.CsvOutput;
 import com.opengamma.strata.collect.timeseries.LocalDateDoubleTimeSeries;
 import com.opengamma.strata.market.observable.QuoteId;
+import com.opengamma.strata.market.param.CurrencyParameterSensitivities;
+import com.opengamma.strata.market.param.CurrencyParameterSensitivity;
+import com.opengamma.strata.market.param.ParameterMetadata;
 
 /**
  * Utilities to export objects (typically in csv files or in the console).
@@ -97,6 +105,62 @@ public class ExportUtils {
         destination.append("," + values[r][c]);
       }
       destination.append("\n");
+    }
+  }
+
+  /**
+   * Append a set of values in an array to a csv-like destination.
+   * 
+   * @param headers  the header of each column
+   * @param values  the values in array format
+   * @param destination  the destination to which the csv-like string is appended
+   * @throws IOException 
+   */
+  public static void exportArray(
+      double[][] values,
+      Appendable destination) throws IOException {
+    int nbRows = values.length;
+    for (int r = 0; r < nbRows; r++) {
+      destination.append("" + values[r][0]);
+      for (int c = 1; c < values[r].length; c++) {
+        destination.append("," + values[r][c]);
+      }
+      destination.append("\n");
+    }
+  }
+  
+  /**
+   * Append a {@link CurrencyParameterSensitivities} to a csv-like destination.
+   * <p>
+   * The sensitivities for each curve are exported one below the other in two columns. One indicating
+   * the metadata and the other one the value. All the values are multiplied by a common factors. This is
+   * to allow the scaling of the internal representation, which is typically as sensitivity to 1, to the
+   * representation used externally, which is typically as sensitivity to 1 basis point (0.0001).
+   * <p>
+   * The metadata should be present for all curves.
+   * 
+   * @param sensitivities  the sensitivities
+   * @param scale  the scaling factor
+   * @param destination  the destination to which the csv-like string is appended
+   */
+  public static void exportCurrencyParameterSensitivities(
+      CurrencyParameterSensitivities sensitivities,
+      double scale,
+      Appendable destination) {
+
+    CsvOutput csv = new CsvOutput(destination);
+    List<CurrencyParameterSensitivity> sensitivitiesAsList = sensitivities.getSensitivities();
+    csv.writeLine(ImmutableList.of("Label", "Value"));
+    for (CurrencyParameterSensitivity sensitivity : sensitivitiesAsList) {
+      csv.writeLine(ImmutableList.of(sensitivity.getMarketDataName().toString(), sensitivity.getCurrency().toString()));
+      ArgChecker.isFalse(sensitivity.getParameterMetadata().isEmpty(), "Parameters metadata required");
+      DoubleArray sensitivityValues = sensitivity.getSensitivity();
+      List<ParameterMetadata> sensitivityMetadata = sensitivity.getParameterMetadata();
+      for (int loopnode = 0; loopnode < sensitivityValues.size(); loopnode++) {
+        csv.writeLine(ImmutableList.of(
+            sensitivityMetadata.get(loopnode).getLabel(),
+            Double.toString((sensitivityValues.get(loopnode) * scale))));
+      }
     }
   }
 
