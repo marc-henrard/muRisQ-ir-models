af148a981218c36c874e6166045a4217b5259019
Marc Henrard <marc@opengamma.com>
2018-02-25 10:35:05 +0100

    Hull-White one-factor formula: convexity adjustment with flexible start and end date.

---
+++ src/main/java/marc/henrard/risq/model/hullwhite/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2018 - present by Marc Henrard.
+ */
+
+/**
+ * Package with classes related to the Hull-White one-factor or extended Vasicek interest rate model.
+ */
+package marc.henrard.risq.model.hullwhite;
+
