913a47c1130d1feb7cebb9464b43787c77ccd967
Marc Henrard <marc@opengamma.com>
2019-11-17 16:10:48 +0100

    More indices

--- src/main/java/marc/henrard/murisq/basics/index/ComplementIborIndices.java
+++ src/main/java/marc/henrard/murisq/basics/index/ComplementIborIndices.java
@@ -25,12 +25,12 @@ public class ComplementIborIndices {
   /** The 6-month SONIA-linked compounding. */
   public static final IborIndex GBP_SONIACMP_6M = IborIndex.of("GBP-SONIACMP-6M");
   
-  /** The 1-month ESTER-linked compounding. */
-  public static final IborIndex EUR_ESTERCMP_1M = IborIndex.of("EUR-ESTERCMP-1M");
-  /** The 3-month ESTER-linked compounding. */
-  public static final IborIndex EUR_ESTERCMP_3M = IborIndex.of("EUR-ESTERCMP-3M");
-  /** The 6-month ESTER-linked compounding. */
-  public static final IborIndex EUR_ESTERCMP_6M = IborIndex.of("EUR-ESTERCMP-6M");
+  /** The 1-month ESTR-linked compounding. */
+  public static final IborIndex EUR_ESTRCMP_1M = IborIndex.of("EUR-ESTRCMP-1M");
+  /** The 3-month ESTR-linked compounding. */
+  public static final IborIndex EUR_ESTRCMP_3M = IborIndex.of("EUR-ESTRCMP-3M");
+  /** The 6-month ESTR-linked compounding. */
+  public static final IborIndex EUR_ESTRCMP_6M = IborIndex.of("EUR-ESTRCMP-6M");
   
   /** The 1-month SOFR-linked compounding. */
   public static final IborIndex USD_SOFRCMP_1M = IborIndex.of("USD-SOFRCMP-1M");
@@ -45,5 +45,26 @@ public class ComplementIborIndices {
   public static final IborIndex USD_FED_FUNDCMP_3M = IborIndex.of("USD-FED-FUNDCMP-3M");
   /** The 6-month ESTER-linked compounding. */
   public static final IborIndex USD_FED_FUNDCMP_6M = IborIndex.of("USD-FED-FUNDCMP-6M");
+  
+  /** The 1-month SONIA-linked OIS. */
+  public static final IborIndex GBP_SONIAOIS_1M = IborIndex.of("GBP-SONIAOIS-1M");
+  /** The 3-month SONIA-linked OIS. */
+  public static final IborIndex GBP_SONIAOIS_3M = IborIndex.of("GBP-SONIAOIS-3M");
+  /** The 6-month SONIA-linked OIS. */
+  public static final IborIndex GBP_SONIAOIS_6M = IborIndex.of("GBP-SONIAOIS-6M");
+  
+  /** The 1-month ESTR-linked OIS. */
+  public static final IborIndex EUR_ESTROIS_1M = IborIndex.of("EUR-ESTROIS-1M");
+  /** The 3-month ESTR-linked OIS. */
+  public static final IborIndex EUR_ESTROIS_3M = IborIndex.of("EUR-ESTROIS-3M");
+  /** The 6-month ESTR-linked OIS. */
+  public static final IborIndex EUR_ESTROIS_6M = IborIndex.of("EUR-ESTROIS-6M");
+  
+  /** The 1-month SOFR-linked OIS. */
+  public static final IborIndex USD_SOFROIS_1M = IborIndex.of("USD-SOFROIS-1M");
+  /** The 3-month ESTER-linked OIS. */
+  public static final IborIndex USD_SOFROIS_3M = IborIndex.of("USD-SOFROIS-3M");
+  /** The 6-month ESTER-linked OIS. */
+  public static final IborIndex USD_SOFROIS_6M = IborIndex.of("USD-SOFROIS-6M");
 
 }
